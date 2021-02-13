143f6ffef2c0f1219131327a3135f5a7f415e685
BeautterLife <pgh9801@gmail.com>
2021-02-13 13:06:50 +0900

    Create 두개뽑아서더하기.py

---
+++ 프로그래머스/월간코드챌린지시즌1/두개뽑아서더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = set()
+    for i,num1 in enumerate(numbers[:-1]):
+        for num2 in numbers[i+1:]:
+            answer.add(num1+num2)
+    #sorted(list)  vs  list.sort()
+    #list.sort()는 안됨 : 정렬된 결과를 반환하지 않음(반환값이 없음)
+    return sorted(list(answer))
