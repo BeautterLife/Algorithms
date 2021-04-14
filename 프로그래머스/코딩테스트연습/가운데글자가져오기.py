46a5b2e9d1e814e7cae4deee3038c6aaab174214
BeautterLife <pgh9801@gmail.com>
2021-04-14 20:21:03 +0900

    Update 가운데글자가져오기.py

--- 프로그래머스/코딩테스트연습/가운데글자가져오기.py
+++ 프로그래머스/코딩테스트연습/가운데글자가져오기.py
@@ -1,3 +1,3 @@
 def solution(s):
-    return s[len(s)//2] if len(s)%2!=0 else s[len(s)//2-1:len(s)//2+1]
+    return s[(len(s)-1)//2:len(s)//2+1] # s[len(s)//2] if len(s)%2!=0 else s[len(s)//2-1:len(s)//2+1]
     
