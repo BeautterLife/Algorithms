f8eb490286ad2a5617292abb8119283b6f1f3d13
BeautterLife <pgh9801@gmail.com>
2021-05-10 15:13:26 +0900

    Update 모의고사.py

--- 프로그래머스/코딩테스트연습/완전탐색/모의고사.py
+++ 프로그래머스/코딩테스트연습/완전탐색/모의고사.py
@@ -30,3 +30,30 @@ def solution(answers):
     #                            new_list = list(map(list, zip(*mylist)))
     
     return sorted(list(zip(*answer))[0])
+
+"""
+def solution(answers):
+    answer = []
+    student_1 = [1,2,3,4,5]
+    student_2 = [2,1,2,3,2,4,2,5]
+    student_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    s_answer=[0,0,0]
+
+    len_s1 = 5
+    len_s2 = 8
+    len_s3 = 10
+
+    for i,answer in enumerate(answers):
+        if student_1[i%len_s1] == answer:
+            s_answer[0]+=1
+        if student_2[i%len_s2] == answer:
+            s_answer[1]+=1
+        if student_3[i%len_s2] == answer:
+            s_answer[2]+=1
+
+    max_answer = max(s_answer)
+    #print(max_answer)
+
+    return [i+1 for i,s_num in enumerate(s_answer) if s_num==max_answer]
+"""
