8fda2281db8fe19eb1d0a992654c1b06177e050f
BeautterLife <pgh9801@gmail.com>
2021-05-13 23:06:45 +0900

    stack

--- 프로그래머스/월간코드챌린지시즌/괄호회전하기.py
+++ 프로그래머스/월간코드챌린지시즌/괄호회전하기.py
@@ -4,18 +4,23 @@ def solution(s):
     p1 = ['(',')']
     p2 = ['{','}']
     p3 = ['[',']']
+    not_p = [')','}',']']
     s = s[-1]+s[0:-1]
 
     for i in range(len(s)):
         s = s[1:]+s[0]
+        
         stack = []
         for p in s:
             stack.append(p)
+            # append했을때 짝지은 괄호가 아니면 올바른 괄호가 아님.
             if len(stack)>=2:
                 if stack[-2:]== p1 or stack[-2:] == p2 or stack[-2:] == p3:
                     stack.pop()
                     stack.pop()
-            
+                    
+                elif p in not_p:
+                    break
         if len(stack)==0:
             answer+=1
     
