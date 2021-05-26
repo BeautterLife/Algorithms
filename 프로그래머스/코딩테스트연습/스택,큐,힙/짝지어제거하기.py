cf8c28afd255c7d73ec53831ce1e139924f515a1
BeautterLife <pgh9801@gmail.com>
2021-05-26 13:36:03 +0900

    Create 짝지어제거하기.py

---
+++ 프로그래머스/코딩테스트연습/스택,큐,힙/짝지어제거하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for c in s:
+        if len(stack)==0:
+            stack.append(c)
+        elif stack[-1]==c:
+            stack.pop()
+        else:
+            stack.append(c)
+    
+    return 1 if len(stack)==0 else 0
