7e42e4bd05d1318c5dafdbe770f346eb4847167a
BeautterLife <pgh9801@gmail.com>
2021-06-26 13:21:22 +0900

    풀이3: dictionary이용 -> 문자열 in / find 보다 빠른 방법

--- 프로그래머스/방학인턴/스킬트리.py
+++ 프로그래머스/방학인턴/스킬트리.py
@@ -1,3 +1,30 @@
+# dictionary 이용한 풀이
+def solution(skill, skill_trees):
+    answer = 0
+
+    for skill_tree in skill_trees:
+        # 스킬트리의 위치를 저장하는 dictionary
+        # skill에 없으면 -1, 있으면 skill에서의 idx를 저장
+        skill_dic = {sk:skill.find(sk) for sk in skill_tree}
+        #스킬을 배울 순서
+        skill_idx = 0
+
+        # skill_tree의 스킬 하나씩 검사
+        for sk in skill_tree:
+            # skill에 있는 스킬이면
+            if skill_dic[sk]>=0:
+                # 해당 순서가 아니면 못배움
+                if skill_dic[sk] != skill_idx:
+                    print(sk, 'd')
+                    break
+                # 스킬 배울 순서를 하나 증가
+                skill_idx+=1
+        
+        # skill_tree의 모든 스킬을 skill 순서를 지켜서 배웠다면 정답
+        else:
+            answer+=1
+    return answer
+
 def solution(skill, skill_trees):
     answer = 0
     
